fixed scatter gradient, removed alpha

convex_adversarial/dual.py
@@ -80,9 +80,9 @@ class DualNetBounds:
             # initialize new terms
             if not self.I_empty[-1]:
                 out_features = self.affine[i+1].out_features
-                subset_eye = x.data.new(self.I[-1].data.sum(), d.numel()).zero_()
-                subset_eye.scatter_(1, self.I[-1].data.nonzero(), d[self.I[-1]].data[:,None])
-                nu.append(self.affine[i+1](Variable(subset_eye)).t())
+                subset_eye = Variable(x.data.new(self.I[-1].data.sum(), d.numel()).zero_())
+                subset_eye.scatter_(1, Variable(self.I[-1].data.nonzero()), d[self.I[-1]][:,None])
+                nu.append(self.affine[i+1](subset_eye).t())
             else:
                 nu.append(Variable(torch.zeros(self.affine[i+1].out_features,1)).type_as(x))         
             gamma.append(self.biases[i+1])
@@ -102,11 +102,10 @@ class DualNetBounds:
                            sum([(self.zl[j][self.I[j]] * nu[j].clamp(min=0)).sum(1) 
                                 for j in range(i+1) if not self.I_empty[j]]))
 
-        self.alpha_ind = [0] + np.cumsum([i.data.sum() for i in self.I]).tolist()
         self.s = [(u/(u-l)) for l,u in zip(self.zl, self.zu)]
         
             
-    def g(self, c, alpha):
+    def g(self, c):
         nu = [[]]*self.k
         nu[-1] = -c
         for i in range(self.k-2,-1,-1):
@@ -115,9 +114,7 @@ class DualNetBounds:
                 nu[i][self.I_neg[i-1].expand_as(nu[i])] = 0
                 if not self.I_empty[i-1]:
                     nu[i][self.I[i-1].expand_as(nu[i])] = (self.s[i-1][self.I[i-1]].repeat(nu[i].size(0)) * 
-                                                          torch.clamp(nu[i][self.I[i-1].expand_as(nu[i])],min=0) + 
-                                                          torch.clamp(nu[i][self.I[i-1].expand_as(nu[i])],max=0) *
-                                                          alpha[:,self.alpha_ind[i-1]:self.alpha_ind[i]].contiguous().view(-1))
+                                                           nu[i][self.I[i-1].expand_as(nu[i])])
         
         f = (-sum(nu[i+1].mm(self.biases[i].view(-1,1))[:,0] for i in range(self.k-1)) 
              - nu[0].mm(self.x.view(-1,1))[:,0]
@@ -125,40 +122,8 @@ class DualNetBounds:
              + sum((nu[i].clamp(min=0)*self.zl[i-1][None,:])[self.I[i-1].expand_as(nu[i])].view(nu[i].size(0),-1).sum(1)
                    for i in range(1,self.k-1) if not self.I_empty[i-1]))
         return f
-    
-    def opt_alpha_default(self, c):
-        all_alpha = [self.s[i][self.I[i]] for i in range(self.k-2) if not self.I_empty[i]]
-        if len(all_alpha) > 0:
-            alpha_ = torch.cat(all_alpha).data
-            alpha = Variable(alpha_[None,:].expand(c.size(0),self.num_alpha), requires_grad=True)
-            return alpha
-        else:
-            return None
-    
-    def opt_alpha_fgs(self, c):
-        alpha = self.opt_alpha_default(c)
-        f = self.g(c, alpha)
-        (-f.sum()).backward(retain_graph=True)
-        alpha = Variable((alpha.grad.data <0).float())
-        return alpha
-    
-    def opt_alpha_adam(self, c, steps=20):
-        alpha = self.opt_alpha_default(c)
-        opt = optim.Adam([alpha], lr=0.5)
-        for i in range(steps):
-            f = self.g(c, alpha)
-            opt.zero_grad()
-            (-f.sum()).backward(retain_graph=True)
-            opt.step()
-            alpha.data.clamp_(min=0, max=1)
-        return Variable(alpha.data)
-    
-    @property
-    def num_alpha(self):
-        return self.alpha_ind[-1]
 
-
-def robust_loss(net, epsilon, X, y, alpha='default'):
+def robust_loss(net, epsilon, X, y):
     num_classes = net[-1].out_features
     ce_loss = 0.0
     err = 0.0
@@ -169,16 +134,7 @@ def robust_loss(net, epsilon, X, y, alpha='default'):
         if X.is_cuda:
             c = c.cuda()
 
-        if alpha == 'default': 
-            a = dual.opt_alpha_default(c)
-        elif alpha == 'fgs': 
-            a = dual.opt_alpha_fgs(c)
-        elif alpha == 'Adam': 
-            a = dual.opt_alpha_adam(c)
-        else:
-            raise ValueError('Unknown alpha type, must be ["default", "fgs", "Adam"]')
-
-        f = -dual.g(c, a)
+        f = -dual.g(c)
         
         err += (f.data.max(0)[1] != y.data[i]).float()
         #hinge_loss += ((Variable(1-torch.eye(num_classes)[:,y[i]]) + f).max()).clamp(min=0)