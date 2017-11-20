9569fcaba93474fc6169a00669543deb508744f5
Eric Wong <ericwong@cs.cmu.edu>
2017-11-19 23:52:50 -0500

    added batched version

--- convex_adversarial/dual.py
+++ convex_adversarial/dual.py
@@ -36,7 +36,7 @@ def full_bias(l, n=None):
     else:
         raise ValueError("Full bias can't be formed for given layer.")
 
-class DualNetBounds:
+class DualNetBounds:    
     def __init__(self, net, x, epsilon):
         x = x.unsqueeze(0)
         # create network without bias terms
@@ -121,6 +121,101 @@ class DualNetBounds:
              - self.epsilon*nu[0].abs().sum(1)
              + sum((nu[i].clamp(min=0)*self.zl[i-1][None,:])[self.I[i-1].expand_as(nu[i])].view(nu[i].size(0),-1).sum(1)
                    for i in range(1,self.k-1) if not self.I_empty[i-1]))
+        return f
+
+class DualNetBoundsBatch: 
+    def __init__(self, net, X, epsilon):
+        self.layers = [l for l in net if isinstance(l, (nn.Linear, nn.Conv2d))]
+        self.affine_transpose = [AffineTranspose(l) for l in self.layers]
+        self.affine = [Affine(l) for l in self.layers]
+        self.k = len(self.layers)+1
+
+        # initialize affine layers with a forward pass
+        _ = X[0].view(1,-1)
+        for a in self.affine: 
+            _ = a(_)
+            
+        self.biases = [full_bias(l, self.affine[i].out_features) 
+                        for i,l in enumerate(self.layers)]
+        
+        gamma = [self.biases[0]]
+        nu = []
+        nu_hat = self.affine[0](X)
+        nu_hat_ = self.affine[0](Variable(torch.eye(self.affine[0].in_features)).type_as(X)).unsqueeze(0)
+        
+        self.zl = [nu_hat + gamma[0] - epsilon*(nu_hat_).abs().sum(1)]
+        self.zu = [nu_hat + gamma[0] + epsilon*(nu_hat_).abs().sum(1)]
+
+        self.I = []
+        self.I_empty = []
+        self.I_neg = []
+        self.I_pos = []
+        self.X = X
+        self.epsilon = epsilon
+        I_collapse = []
+        I_ind = []
+
+        for i in range(0,self.k-2):
+            # compute sets and activation
+            self.I_neg.append(self.zu[-1] <= 0)
+            self.I_pos.append(self.zl[-1] > 0)
+            self.I.append((self.zu[-1] > 0) * (self.zl[-1] < 0))
+            self.I_empty.append(self.I[-1].data.long().sum() == 0)
+            d = self.I_pos[-1].type_as(X) + (self.zu[-1]/(self.zu[-1] - self.zl[-1]))*self.I[-1].type_as(X)
+
+            # indices of [example idx, origin crossing feature idx]
+            I_ind.append(self.I[-1].data.nonzero())
+            
+            # initialize new terms
+            if not self.I_empty[-1]:
+                out_features = self.affine[i+1].out_features
+
+                subset_eye = Variable(X.data.new(self.I[-1].data.sum(), d.size(1)).zero_())
+                subset_eye.scatter_(1, Variable(I_ind[-1][:,1,None]), d[self.I[-1]][:,None])
+                nu.append(self.affine[i+1](subset_eye).t())
+            else:
+                nu.append(Variable(torch.zeros(self.affine[i+1].out_features,1)).type_as(x))         
+            gamma.append(self.biases[i+1])
+            # propagate terms
+            gamma[0] = self.affine[i+1](d * gamma[0])
+            for j in range(1,i+1):
+                gamma[j] = self.affine[i+1](d * gamma[j])
+                nu[j-1] = self.affine[i+1]((d[I_ind[j-1][:,0]].t() * nu[j-1]).t()).t()
+
+            nu_hat = self.affine[i+1](d*nu_hat)
+            nu_hat_ = self.affine[i+1](d.unsqueeze(1)*nu_hat_)
+
+            # create a matrix that collapses the minibatch of origin-crossing indices 
+            # back to the sum of each minibatch
+            I_collapse.append(Variable(X.data.new(I_ind[-1].size(0), X.size(0))))
+            I_collapse[-1].scatter_(1, I_ind[-1][:,0][:,None], 1)
+
+            # compute bounds
+            self.zl.append(nu_hat + sum(gamma) - epsilon*nu_hat_.abs().sum(1) + 
+                           sum([(self.zl[j][self.I[j]] * (-nu[j]).clamp(min=0)).mm(I_collapse[j]).t()
+                                for j in range(i+1) if not self.I_empty[j]]))
+            self.zu.append(nu_hat + sum(gamma) + epsilon*nu_hat_.abs().sum(1) - 
+                           sum([(self.zl[j][self.I[j]] * nu[j].clamp(min=0)).mm(I_collapse[j]).t()
+                                for j in range(i+1) if not self.I_empty[j]]))
+
+        self.s = [(u/(u-l)) for l,u in zip(self.zl, self.zu)]
+        
+    def g(self, c):
+        nu = [[]]*self.k
+        nu[-1] = -c
+        for i in range(self.k-2,-1,-1):
+            nu[i] = self.affine_transpose[i](nu[i+1].view(-1, nu[i+1].size(2))).view(c.size(0), c.size(1), -1)
+            if i > 0:
+                nu[i][self.I_neg[i-1].unsqueeze(1)] = 0
+                if not self.I_empty[i-1]:
+                    nu[i][self.I[i-1].unsqueeze(1)] = (self.s[i-1][self.I[i-1]].unsqueeze(1).repeat(1,nu[i].size(1)).view(-1) * 
+                                                           nu[i][self.I[i-1].unsqueeze(1)])
+
+        f = (-sum(nu[i+1].matmul(self.biases[i].view(-1)) for i in range(self.k-1))
+             -nu[0].matmul(self.X.unsqueeze(2)).squeeze(2)
+             -self.epsilon*nu[0].abs().sum(2)
+             + sum((nu[i].clamp(min=0)*self.zl[i-1].unsqueeze(1)).matmul(self.I[i-1].type_as(self.X).unsqueeze(2)).squeeze(2) 
+                    for i in range(1, self.k-1) if not self.I_empty[i-1]))
         return f
 
 def robust_loss(net, epsilon, X, y):
