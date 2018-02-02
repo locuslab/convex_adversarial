d6028c421e9bae8356a8420d591a07659d045cc2
Eric Wong <ericwong@cs.cmu.edu>
2018-02-02 17:24:22 -0500

    updated mnist example

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,5 +1,5 @@
 import waitGPU
-waitGPU.wait(utilization=80, available_memory=11000)
+waitGPU.wait(utilization=20, available_memory=10000, interval=10)
 
 import torch
 import torch.nn as nn
@@ -26,11 +26,15 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', type=int, default=50)
     parser.add_argument('--epochs', type=int, default=20)
     parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--verbose', type=int, default=1)
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--epsilon", type=float, default=0.1)
+    parser.add_argument("--starting_epsilon", type=float, default=None)
     parser.add_argument('--prefix')
+    parser.add_argument('--baseline', action='store_true')
     parser.add_argument('--alpha_grad', action='store_true')
     parser.add_argument('--scatter_grad', action='store_true')
+    parser.add_argument('--old_weights', action='store_true')
     parser.add_argument('--l1_proj', type=int, default=None)
     args = parser.parse_args()
     args.prefix = args.prefix or 'mnist_conv_{:.4f}_{:.4f}_0'.format(args.epsilon, args.lr).replace(".","_")
@@ -57,11 +61,23 @@ if __name__ == "__main__":
         nn.ReLU(),
         nn.Linear(100, 10)
     ).cuda()
+    
+    if args.old_weights: 
+        model.load_state_dict(torch.load('torch_0_2_weights.pth'))
 
     opt = optim.Adam(model.parameters(), lr=args.lr)
     for t in range(args.epochs):
-        train_robust(train_loader, model, opt, args.epsilon, t, train_log, 
-            10, 
-            args.alpha_grad, args.scatter_grad, l1_proj=args.l1_proj)
-        evaluate_robust(test_loader, model, args.epsilon, t, test_log, 10)
+        if args.baseline: 
+            train_baseline(train_loader, model, opt, t, train_log, args.verbose)
+            evaluate_baseline(test_loader, model, t, test_log, args.verbose)
+        else:
+            if t <= args.epochs//2 and args.starting_epsilon is not None: 
+                epsilon = args.starting_epsilon + (t/(args.epochs//2))*(args.epsilon - args.starting_epsilon)
+            else:
+                epsilon = args.epsilon
+            train_robust(train_loader, model, opt, epsilon, t, train_log, 
+                args.verbose, 
+                args.alpha_grad, args.scatter_grad, l1_proj=args.l1_proj)
+            evaluate_robust(test_loader, model, args.epsilon, t, test_log, args.verbose)
+
         torch.save(model.state_dict(), args.prefix + "_model.pth")
