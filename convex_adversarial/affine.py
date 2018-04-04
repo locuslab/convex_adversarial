d82ec8de46cdc8103c67e3f7f9d66aad517e5e78
Eric Wong <ericwong@cs.cmu.edu>
2018-04-04 18:11:46 -0400

    removed redundant contiguous calls

--- convex_adversarial/affine.py
+++ convex_adversarial/affine.py
@@ -5,6 +5,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 
 def conv2d(x, *args, **kwargs): 
+    """ Minibatched inputs to conv2d """
     i = 0
     out = []
     batch_size = 10000
@@ -13,7 +14,7 @@ def conv2d(x, *args, **kwargs):
         i += batch_size
     return torch.cat(out, 0)
 
-def conv_transpose2d(x, *args, **kwargs): 
+def conv_transpose2d(x, *args, **kwargs):
     i = 0
     out = []
     batch_size = 10000
@@ -29,12 +30,12 @@ def convert2to4(x, C, transpose=False):
     m,n = x.size()
     channels = C.in_channels if not transpose else C.out_channels
     k = int((n//channels)**0.5)
-    return x.contiguous().view(m,channels,k,k)
+    return x.view(m,channels,k,k)
 
 # Convert convolutional input to flattened input
 def convert4to2(x): 
     m = x.size(0)
-    return x.contiguous().view(m,-1)
+    return x.view(m,-1)
 
 class Affine(): 
     def __init__(self): 
@@ -67,9 +68,6 @@ class AffineLinear(Affine):
         self.record_size(x,out)
         return out
 
-    def l1(self): 
-        return self.l.weight.abs().sum(1)
-
 class AffineConv2d(Affine): 
     def __init__(self, l): 
         super(AffineConv2d,self).__init__()
@@ -80,19 +78,12 @@ class AffineConv2d(Affine):
     def forward(self, x): 
         if x.dim() == 2: 
             x = convert2to4(x, self.l)
-        out = conv2d(x.contiguous(), self.l.weight, 
+        out = conv2d(x, self.l.weight, 
                        stride=self.l.stride,
                        padding=self.l.padding)
         out = convert4to2(out)
         self.record_size(x,out)
         return out
-
-    def l1(self): 
-        if self.out_features is None:
-            raise ValueError("Affine layer not initialized.")
-        oc, of = self.l.out_channels, self.out_features
-        weight_sums = self.l.weight.view(self.l.out_channels, -1).abs().sum(1)
-        return weight_sums[:,None].expand(oc,of//oc).contiguous().view(-1)
 
 class AffineTransposeLinear(nn.Module): 
     def __init__(self, l): 
@@ -116,7 +107,7 @@ class AffineTransposeConv2d(nn.Module):
     def forward(self, x): 
         if x.dim() == 2: 
             x = convert2to4(x, self.l, transpose=True)
-        out = conv_transpose2d(x.contiguous(), self.l.weight, 
+        out = conv_transpose2d(x, self.l.weight, 
                                  stride=self.l.stride,
                                  padding=self.l.padding)
         return convert4to2(out)
