b944a57631b5fcd3edae7e69822361dfb2e088ce
Eric Wong <ericwong@cs.cmu.edu>
2018-04-17 22:50:46 -0400

    added cifar example

---
+++ examples/cifar.py
@@ -0,0 +1,97 @@
+# import setGPU
+# waitGPU.wait(utilization=20, available_memory=10000, interval=60)
+# waitGPU.wait(gpu_ids=[1,3], utilization=20, available_memory=10000, interval=60)
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import setproctitle
+
+import problems as pblm
+from trainer import *
+
+if __name__ == "__main__": 
+    args = pblm.argparser()
+    print("saving file to {}".format(args.prefix))
+    setproctitle.setproctitle(args.prefix)
+
+    if not args.eval:
+        train_log = open(args.prefix + "_train.log", "w")
+    test_log = open(args.prefix + "_test.log", "w")
+
+    train_loader, test_loader = pblm.cifar_loaders(args.batch_size)
+    # if args.vgg: 
+    #     _, test_loader = pblm.mnist_loaders(1, shuffle_test=True)
+    #     test_loader = [tl for i,tl in enumerate(test_loader) if i < 200]
+
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    if args.vgg: 
+        # raise ValueError
+        model = pblm.cifar_model_vgg().cuda()
+    elif args.large: 
+        raise ValueError
+        #model = pblm.mnist_model_large().cuda()
+    else: 
+        model = pblm.cifar_model().cuda() 
+        #model.load_state_dict(torch.load('l1_truth/mnist_nonexact_rerun_baseline_False_batch_size_50_delta_0.01_epochs_20_epsilon_0.1_l1_proj_200_l1_test_exact_l1_train_median_lr_0.001_m_10_seed_0_starting_epsilon_0.05_model.pth'))
+
+    # for X,y in train_loader: 
+    #     X = Variable(X.cuda())
+    #     break
+    # print(X.max(), X.min())
+    # print(model(X).size())
+    # assert False
+
+    kwargs = pblm.args2kwargs(args)
+    best_err = 1
+    best_state_dict = model.state_dict()
+
+    if args.eval is not None: 
+        try: 
+            model.load_state_dict(torch.load(args.eval))
+        except:
+            print('[Warning] eval argument could not be loaded, evaluating a random model')
+        evaluate_robust(test_loader, model, args.epsilon, 0, test_log,
+            args.verbose, 
+              **kwargs)
+    else: 
+        opt = optim.Adam(model.parameters(), lr=args.lr)
+        eps_schedule = np.logspace(np.log10(args.starting_epsilon), 
+                                   np.log10(args.epsilon), 
+                                   args.epochs//2)
+        for t in range(args.epochs):
+            if t > 0 and t % 30 == 0: 
+                args.lr /= 2
+            if args.baseline: 
+                train_baseline(train_loader, model, opt, t, train_log, args.verbose)
+                err = evaluate_baseline(test_loader, model, t, test_log, args.verbose)
+            else:
+                if t < args.epochs//2 and args.starting_epsilon is not None: 
+                    # epsilon = args.starting_epsilon + (t/(args.epochs//2))*(args.epsilon - args.starting_epsilon)
+                    epsilon = float(eps_schedule[t])
+                else:
+                    epsilon = args.epsilon
+                train_robust(train_loader, model, opt, epsilon, t, train_log, 
+                    args.verbose, l1_type=args.l1_train, **kwargs)
+                err = evaluate_robust(test_loader, model, args.epsilon, t, test_log,
+                   args.verbose, l1_type=args.l1_test, **kwargs)
+                      # l1_geometric=args.l1_proj)
+            
+            if err < best_err: 
+                best_state_dict = model.state_dict()
+                best_err = err
+                
+            torch.save({ 
+                'state_dict': model.state_dict(),
+                'err' : err,
+                'best_state_dict' : best_state_dict, 
+                'best_err' : best_err,
+                'epoch' : t
+                }, args.prefix + "_checkpoint.pth")
