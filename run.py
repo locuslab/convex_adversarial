bffccc48272182a560139639c4a28a1664509559
Eric Wong <ericwong@cs.cmu.edu>
2018-05-09 04:55:43 -0400

    fixed docker run script

--- run.py
+++ run.py
@@ -1,76 +1,24 @@
 import os
 import subprocess
-# uid, gid = os.system('id -u'), os.system('id -g')
-subprocess.run(['docker', 'build',  '-qt', 'convex_adversarial', '.'])
+import argparse
 
-uid = int(subprocess.run(['id', '-u'], stdout=subprocess.PIPE).stdout)
-gid = int(subprocess.run(['id', '-g'], stdout=subprocess.PIPE).stdout)
-
-# gpu_id = 0
-# gpu_id = 1
-# gpu_id = 2
-gpu_id = 2
-
-# mnist-vgg exact
-# name = 'mnist'
-# epochs = 20
-# lr=0.001
-# epsilon = 0.01
-# starting_epsilon=1e-3
-# verbose = 100
-# batch_size=4
-# model_type='--vgg'
-# baseline=''
-# extra = ''
-
-# baseline cifar-vgg
-# name = 'cifar'
-# epochs = 300
-# lr=0.05
-# verbose = 20
-# epsilon = 0.031
-# starting_epsilon=0.001
-# batch_size=128
-# baseline = '--baseline'
-# model_type = '--vgg_pytorch'
-# extra = ''
-
-# baseline cifar-resnet
-name = 'cifar'
-epochs = 300
-lr=0.01
-verbose = 100
-epsilon = 0.031
-starting_epsilon=0.001
-batch_size=128
-baseline = '--baseline'
-model_type = '--resnet_bn'
-extra = ''
+def docker_run(cmd): 
+    uid = int(subprocess.run(['id', '-u'], stdout=subprocess.PIPE).stdout)
+    gid = int(subprocess.run(['id', '-g'], stdout=subprocess.PIPE).stdout)
+    subprocess.run(['docker', 'build',  '-qt', 'convex_adversarial', '.'])
 
 
+    s = """docker run -it --runtime=nvidia --rm -w /home -v ${{PWD}}/nips/:/home/nips/ convex_adversarial zsh -c '{}; {}'"""
+    fix_permissions = 'chown -R {}:{} /home/nips/'.format(uid, gid)
 
-# baseline = ''
+    os.system(s.format(cmd, fix_permissions))
 
-# different model types
-# model_type = '--resnet'
+if __name__ == "__main__":
+    uid = int(subprocess.run(['id', '-u'], stdout=subprocess.PIPE).stdout)
+    gid = int(subprocess.run(['id', '-g'], stdout=subprocess.PIPE).stdout)
 
-
-
-s = """docker run -it --runtime=nvidia --rm -w /home -v ${{PWD}}/experiments/:/experiments/ convex_adversarial zsh -c '{}; {}; {}'"""
-cmd = 'python examples/{0}.py --prefix /experiments/{0}{extra} {baseline} --lr {lr} --epochs {epochs} --verbose {verbose} {model_type} --epsilon {epsilon} --starting_epsilon {starting_epsilon} --batch_size {batch_size}'.format(
-    name, 
-    lr=lr,
-    verbose=verbose,
-    epochs=epochs, 
-    model_type=model_type,
-    baseline=baseline,
-    epsilon=epsilon,
-    starting_epsilon=starting_epsilon,
-    extra=extra, 
-    batch_size=batch_size
-    )
-set_gpu = 'export CUDA_VISIBLE_DEVICES={}'.format(gpu_id)
-fix_permissions = 'chown -R {}:{} /experiments'.format(uid, gid)
-
-full = s.format(set_gpu, cmd, fix_permissions)
-os.system(full)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd', type=str, help='command to run')
+    
+    args = parser.parse_args()
+    docker_run(args.cmd)
