092d097d7519c22b239e6159ce56a35bf5f27ae1
Eric Wong <riceric22@gmail.com>
2018-05-14 20:01:39 -0400

    added mnist cascades

--- examples/mnist.py
+++ examples/mnist.py
@@ -107,48 +107,103 @@ if __name__ == "__main__":
                             weight_decay=args.weight_decay)
         else: 
             raise ValueError("Unknown optimizer")
-        lr_scheduler = optim.lr_scheduler.StepLR(opt, step_size=20, gamma=0.5)
-        # schedule epsilon over a maximum of 20 epochs
-        eps_schedule = np.logspace(np.log10(args.starting_epsilon), 
-                                   np.log10(args.epsilon), 
-                                   args.schedule_length)
-        for t in range(args.epochs):
-            # reduce LR after epsilon schedule
-            lr_scheduler.step(epoch=max(t-len(eps_schedule), 0))
-            if args.method == 'baseline': 
-                train_baseline(train_loader, model, opt, t, train_log, args.verbose)
-                err = evaluate_baseline(test_loader, model, t, test_log,
-                   args.verbose)
-            elif args.method=='madry':
-                train_madry(train_loader, model, args.epsilon, opt, t, train_log,
-                   args.verbose)
-                evaluate_madry(test_loader, model, args.epsilon, t, test_log,
-                   args.verbose)
-            else:
-                if t < len(eps_schedule) and args.starting_epsilon is not None: 
-                    # epsilon = args.starting_epsilon + (t/(args.epochs//2))*(args.epsilon - args.starting_epsilon)
-                    epsilon = float(eps_schedule[t])
-                else:
-                    epsilon = args.epsilon
-                train_robust(train_loader, model, opt, epsilon, t, train_log, 
-                    args.verbose, l1_type=args.l1_train, bounded_input=True, 
-                    **kwargs)
-                err = evaluate_robust(test_loader, model, args.epsilon, t,
-                   test_log,
-                   args.verbose, l1_type=args.l1_test, bounded_input=True, 
-                   **kwargs)
-            
-            if err < best_err: 
-                best_err = err
-                torch.save({
-                    'state_dict' : model.state_dict(), 
-                    'err' : best_err,
-                    'epoch' : t
-                    }, args.prefix + "_best.pth")
+
+        if args.cascade: 
+            model = [model]
+            print('cascade training ')
+            for _ in range(args.cascade): 
+                if _ > 0: 
+                    print("Loading best model")
+                    d = torch.load(args.prefix+"_best.pth")
+                    model[-1].load_state_dict(d['state_dict'][-1])
+                    
+                    print("Adding a new model")
+                    model.append(pblm.mnist_model().cuda())
+                    # also reduce dataset to just uncertified examples
+                    train_loader = sampler_robust_cascade(train_loader, model, args.epsilon, **kwargs)
+                    if train_loader is None: 
+                        break
                 
-            torch.save({ 
-                'state_dict': model.state_dict(),
-                'err' : err,
-                'epoch' : t
-                }, args.prefix + "_checkpoint.pth")
-            # torch.save(model.state_dict(), args.prefix + "_model.pth")
+                opt = optim.Adam(model[-1].parameters(), lr=args.lr)
+                # opt = optim.SGD(model[-1].parameters(), lr=args.lr, momentum=args.momentum,
+                #     weight_decay=args.weight_decay)
+                lr_scheduler = optim.lr_scheduler.StepLR(opt, step_size=10, gamma=0.5)
+                eps_schedule = np.logspace(np.log10(args.starting_epsilon), 
+                                           np.log10(args.epsilon), 
+                                           args.schedule_length)
+
+                for t in range(args.epochs):
+                    lr_scheduler.step(epoch=max(t-len(eps_schedule), 0))
+                    if t < len(eps_schedule) and args.starting_epsilon is not None: 
+                        # epsilon = args.starting_epsilon + (t/(args.epochs//2))*(args.epsilon - args.starting_epsilon)
+                        epsilon = float(eps_schedule[t])
+                    else:
+                        epsilon = args.epsilon
+                    train_robust(train_loader, model[-1], opt, epsilon, t, train_log, 
+                                    args.verbose, l1_type=args.l1_train, bounded_input=True,  **kwargs)
+                    # train_robust_cascade(train_loader, model, opt, epsilon, t, train_log, 
+                    #     args.verbose, l1_type=args.l1_train, **kwargs)
+                    err = evaluate_robust_cascade(test_loader, model, args.epsilon, t, test_log,
+                       args.verbose, l1_type=args.l1_test, bounded_input=True,  **kwargs)
+
+                    
+                    if err < best_err: 
+                        best_err = err
+                        torch.save({
+                            'state_dict' : [m.state_dict() for m in model], 
+                            'err' : best_err,
+                            'epoch' : t
+                            }, args.prefix + "_best.pth")
+                        
+                    torch.save({ 
+                        'state_dict': [m.state_dict() for m in model],
+                        'err' : err,
+                        'epoch' : t
+                        }, args.prefix + "_checkpoint.pth")
+        else:
+            lr_scheduler = optim.lr_scheduler.StepLR(opt, step_size=20, gamma=0.5)
+            # schedule epsilon over a maximum of 20 epochs
+            eps_schedule = np.logspace(np.log10(args.starting_epsilon), 
+                                       np.log10(args.epsilon), 
+                                       args.schedule_length)
+
+            for t in range(args.epochs):
+                # reduce LR after epsilon schedule
+                lr_scheduler.step(epoch=max(t-len(eps_schedule), 0))
+                if args.method == 'baseline': 
+                    train_baseline(train_loader, model, opt, t, train_log, args.verbose)
+                    err = evaluate_baseline(test_loader, model, t, test_log,
+                       args.verbose)
+                elif args.method=='madry':
+                    train_madry(train_loader, model, args.epsilon, opt, t, train_log,
+                       args.verbose)
+                    evaluate_madry(test_loader, model, args.epsilon, t, test_log,
+                       args.verbose)
+                else:
+                    if t < len(eps_schedule) and args.starting_epsilon is not None: 
+                        # epsilon = args.starting_epsilon + (t/(args.epochs//2))*(args.epsilon - args.starting_epsilon)
+                        epsilon = float(eps_schedule[t])
+                    else:
+                        epsilon = args.epsilon
+                    train_robust(train_loader, model, opt, epsilon, t, train_log, 
+                        args.verbose, l1_type=args.l1_train, bounded_input=True, 
+                        **kwargs)
+                    err = evaluate_robust(test_loader, model, args.epsilon, t,
+                       test_log,
+                       args.verbose, l1_type=args.l1_test, bounded_input=True, 
+                       **kwargs)
+                
+                if err < best_err: 
+                    best_err = err
+                    torch.save({
+                        'state_dict' : model.state_dict(), 
+                        'err' : best_err,
+                        'epoch' : t
+                        }, args.prefix + "_best.pth")
+                    
+                torch.save({ 
+                    'state_dict': model.state_dict(),
+                    'err' : err,
+                    'epoch' : t
+                    }, args.prefix + "_checkpoint.pth")
+                # torch.save(model.state_dict(), args.prefix + "_model.pth")
