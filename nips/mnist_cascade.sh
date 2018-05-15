0e124a8fe27facd85e6a9880d50337aaf95c20ff
Eric Wong <riceric22@gmail.com>
2018-05-14 21:21:34 -0400

    added cascade training scripts

---
+++ nips/mnist_cascade.sh
@@ -0,0 +1,21 @@
+if [ "${1}" != "" ] && [ "${2}" != "" ]; then
+     directory=`basename $0 .sh`
+     prefix="nips/${directory}"
+
+     mkdir -p $prefix
+
+     python examples/cifar.py --prefix ${prefix}/cifar \
+                              --cascade 10 \
+                              --l1_proj 50 \
+                              --l1_train median \
+                              --epsilon ${2} \
+                              --starting_epsilon 0.01 \
+                              --epochs 100 \
+                              --batch_size 50 \
+                              --schedule_length 20 \
+                              --opt adam \
+                              --verbose 200 \
+                              --cuda_ids ${1}
+else
+    echo "Error: need to pass in GPU ids and epsilon to run script."
+fi
