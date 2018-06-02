0ce8ff6cb07f7b4d845de84bb8579e8492492d17
Eric <ericwong@cs.cmu.edu>
2018-06-02 02:19:38 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='convex_adversarial',
-    version='0.3.1',
+    version='0.3.2',
     description="A library implementing robust loss functions for ReLU based neural networks. ",
     author='Eric Wong',
     author_email='ericwong@cs.cmu.edu',
