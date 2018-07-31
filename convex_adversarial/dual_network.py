b6f3d6dbaa93e19f65d6cb600dda2f4243f335d9
Eric <ericwong@cs.cmu.edu>
2018-07-31 15:10:57 -0400

    working dataprallel loss function

--- convex_adversarial/dual_network.py
+++ convex_adversarial/dual_network.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 
-from .utils import Dense, DenseSequential, transpose_all
+from .utils import Dense, DenseSequential
 from .dual_inputs import select_input
 from .dual_layers import select_layer
 
@@ -110,55 +110,23 @@ def robust_loss(net, epsilon, X, y,
     ce_loss = nn.CrossEntropyLoss(reduce=size_average)(f, y)
     return ce_loss, err
 
+class InputSequential(nn.Sequential): 
+    def __init__(self, *args, **kwargs): 
+        self.i = 0
+        super(InputSequential, self).__init__(*args, **kwargs)
+
+    def set_start(self, i): 
+        self.i = i
+
+    def forward(self, input): 
+        """ Helper class to apply a sequential model starting at the ith layer """
+        for j,module in enumerate(self._modules.values()): 
+            if j >= self.i: 
+                input = module(input)
+        return input
+
+
 # Data parallel versions of the loss calculation
-# Warning: experimental
-
-class DualSequential(nn.Module): 
-    def __init__(self, dual_layers, net): 
-        super(DualSequential, self).__init__()
-        self.dual_layers = nn.ModuleList(dual_layers)
-        self.net = net
-        pass
-    def forward(self, x, I_ind=None): 
-        zs = [x]
-        for l in list(self.dual_layers)[1:]: 
-            if isinstance(l, DualDense): 
-                zs.append(l(*zs))
-            elif isinstance(l, DualReLU): 
-                zs.append(l(zs[-1], I_ind=I_ind))
-            else:
-                zs.append(l(zs[-1]))
-        return zs[-1]
-
-def dual_helper(dual_layer, D): 
-    if isinstance(dual_layer, (DualLinear, DualConv2d)): 
-        b = dual_layer.bias[0]
-        if isinstance(dual_layer, DualConv2d): 
-            b = b.unsqueeze(0)
-        Db = D(b)
-        return Db, Db
-    elif isinstance(dual_layer, DualReshape):
-        return 0,0
-    elif isinstance(dual_layer, DualReLU):
-        if dual_layer.I_empty: 
-            return 0,0
-        D = nn.DataParallel(D)
-        nu0 = D(dual_layer.nus[0], I_ind=dual_layer.I_ind)
-
-        nu = nu0.view(nu0.size(0), -1)
-        zlI = dual_layer.zl[dual_layer.I]
-        zl = (zlI * (-nu.t()).clamp(min=0)).mm(dual_layer.I_collapse).t().contiguous()
-        zu = -(zlI * nu.t().clamp(min=0)).mm(dual_layer.I_collapse).t().contiguous()
-        
-        return zl.view(-1, *(nu0.size()[1:])), zu.view(-1, *(nu0.size()[1:]))
-    elif isinstance(dual_layer, DualDense): 
-        fvals = [dual_helper(d, D) for d in dual_layer.duals if d is not None]
-        l,u = zip(*fvals)
-        return sum(l), sum(u)
-    else: 
-        print(dual_layer)
-        raise NotImplementedError
-
 def robust_loss_parallel(net, epsilon, X, y, l1_proj=None, 
                  l1_type='exact', bounded_input=False, size_average=True): 
     if any('BatchNorm2d' in str(l.__class__.__name__) for l in net): 
@@ -180,46 +148,24 @@ def robust_loss_parallel(net, epsilon, X, y, l1_proj=None,
 
     dual_net = [select_input(X, epsilon, l1_proj, l1_type, bounded_input)]
 
-    if any(isinstance(l, Dense) for l in net): 
-        dense_t = Aff.transpose_all(net)
-    else: 
-        dense_t = [None]*len(net)
-
-    eye = dual_net[0].nu_1[0]
-    x = dual_net[0].nu_x[0]
-
     for i,(in_f,out_f,layer) in enumerate(zip(nf[:-1], nf[1:], net)): 
         if isinstance(layer, nn.ReLU): 
             # compute bounds
-            D =nn.DataParallel(DualSequential(dual_net, net))
+            D = (InputSequential(*dual_net[1:]))
+            zl,zu = 0,0
+            for j,dual_layer in enumerate(dual_net): 
+                D.set_start(j)
+                out = dual_layer.bounds(network=D)
+                zl += out[0]
+                zu += out[1]
 
-            # should be negative, but doesn't matter with abs()
-            nu_1 = D(dual_net[0].nu_1[0]).abs().sum(1)
-            nu_x = D(dual_net[0].nu_x[0])
-
-            rest = 0
-            rest_l = 0
-            rest_u = 0
-            for i,dual_layer in enumerate(dual_net[1:]): 
-                D = DualSequential(dual_net[i+1:], net)
-                out = dual_helper(dual_layer, D)
-                rest_l += out[0]
-                rest_u += out[1]
-
-            zl = nu_x - epsilon*nu_1 + rest_l
-            zu = nu_x + epsilon*nu_1 + rest_u
-
-            d = (zl >= 0).detach().type_as(X)
-            I = ((zu > 0).detach() * (zl < 0).detach())
-            if I.sum().item() > 0:
-                d[I] += zu[I]/(zu[I] - zl[I])
-
-            dual_layer = DualReLU(I, d, zl)
+            dual_layer = select_layer(layer, dual_net, X, l1_proj, l1_type,
+                in_f, out_f, zs[i], zl=zl, zu=zu)
         else:
-            dual_layer = select_layer(layer, dual_net, X, l1_proj, l1_type, in_f, out_f, dense_t[i], zs[i])
+            dual_layer = select_layer(layer, dual_net, X, l1_proj, l1_type,
+                in_f, out_f, zs[i])
         
         dual_net.append(dual_layer)
-
 
     num_classes = net[-1].out_features
     c = Variable(torch.eye(num_classes).type_as(X)[y].unsqueeze(1) - torch.eye(num_classes).type_as(X).unsqueeze(0))
@@ -231,10 +177,8 @@ def robust_loss_parallel(net, epsilon, X, y, l1_proj=None,
     for l in reversed(dual_net[1:]): 
         nu.append(l.T(*nu))
     
-    nu.append(None)
-    nu = list(reversed(nu))
-    f = -sum(l.fval(nu=n, nu_prev=nprev) 
-        for l,nprev,n in zip(dual_net, nu[:-1],nu[1:]))
+    f = -sum(l.objective(*nu[:min(len(dual_net)-i+1, len(dual_net))]) 
+             for i,l in enumerate(dual_net))
 
     err = (f.max(1)[1] != y)
 
