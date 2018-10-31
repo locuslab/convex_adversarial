8073eb1bbe950dc0541d226799d5e86afad34e51
Eric <ericwong@cs.cmu.edu>
2018-10-30 20:13:03 -0400

    added DualNetwork to __init__

--- convex_adversarial/__init__.py
+++ convex_adversarial/__init__.py
@@ -1,2 +1,2 @@
-from .dual_network import DualNetBounds, robust_loss, robust_loss_parallel
+from .dual_network import DualNetBounds, robust_loss, robust_loss_parallel, DualNetwork
 from .utils import DenseSequential, Dense, epsilon_from_model
