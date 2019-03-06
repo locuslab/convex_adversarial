ac8d100e6480d45654d895cd876d33d7b2736624
Eric <ericwong@cs.cmu.edu>
2019-03-06 17:44:41 -0500

    bump version, free port to pytorch 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='convex_adversarial',
-    version='0.4.0',
+    version='0.4.1',
     description="A library implementing robust loss functions for neural networks. ",
     author='Eric Wong',
     author_email='ericwong@cs.cmu.edu',
@@ -11,6 +11,6 @@ setup(
     url='https://github.com/locuslab/convex_adversarial',
     packages=['convex_adversarial'],
     install_requires=[
-        'torch==0.4'
+        'torch>=0.4'
     ]
 )
